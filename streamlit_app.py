aac6b590aa99770740204aa6c9cf1a820a6db7f0
Leonardo Alves <leonardo.uern@hotmail.com>
2023-01-19 00:05:12 -0300

    Commit 6

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,13 +44,10 @@ def importarModelo(modeloEscolhido):
     return criarModelo(modeloEscolhido, base01, alvo)
 
 
-modeloET = ExtraTreesRegressor(bootstrap=False, ccp_alpha=0.0, criterion='mse',
-                               max_depth=None, max_features='auto', max_leaf_nodes=None,
-                               max_samples=None, min_impurity_decrease=0.0,
-                               min_impurity_split=None, min_samples_leaf=1,
-                               min_samples_split=2, min_weight_fraction_leaf=0.0,
-                               n_estimators=100, n_jobs=-1, oob_score=False,
-                               random_state=2444, verbose=0, warm_start=False)
+modeloET = ExtraTreesRegressor(C=1.0, class_weight=None, dual=False, fit_intercept=True,
+                               intercept_scaling=1, l1_ratio=None, max_iter=1000, multi_class='auto',
+                               n_jobs=None, penalty='l2', random_state=1340, solver='lbfgs',
+                               tol=0.0001, verbose=0, warm_start=False)
 
 modeloGB = GradientBoostingRegressor(alpha=0.9, ccp_alpha=0.0, criterion='friedman_mse',
                                      init=None, learning_rate=0.1, loss='ls', max_depth=3,
