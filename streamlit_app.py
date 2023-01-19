7654d618f93b6764c499fae46dce4d97b5afcdbe
Leonardo Alves <leonardo.uern@hotmail.com>
2023-01-18 23:59:43 -0300

    Quinto Commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,30 +45,30 @@ def importarModelo(modeloEscolhido):
 
 
 modeloET = ExtraTreesRegressor(bootstrap=False, ccp_alpha=0.0, criterion='mse',
-                    max_depth=None, max_features='auto', max_leaf_nodes=None,
-                    max_samples=None, min_impurity_decrease=0.0,
-                    min_impurity_split=None, min_samples_leaf=1,
-                    min_samples_split=2, min_weight_fraction_leaf=0.0,
-                    n_estimators=100, n_jobs=-1, oob_score=False,
-                    random_state=2444, verbose=0, warm_start=False)
+                               max_depth=None, max_features='auto', max_leaf_nodes=None,
+                               max_samples=None, min_impurity_decrease=0.0,
+                               min_impurity_split=None, min_samples_leaf=1,
+                               min_samples_split=2, min_weight_fraction_leaf=0.0,
+                               n_estimators=100, n_jobs=-1, oob_score=False,
+                               random_state=2444, verbose=0, warm_start=False)
 
 modeloGB = GradientBoostingRegressor(alpha=0.9, ccp_alpha=0.0, criterion='friedman_mse',
-                          init=None, learning_rate=0.1, loss='ls', max_depth=3,
-                          max_features=None, max_leaf_nodes=None,
-                          min_impurity_decrease=0.0, min_impurity_split=None,
-                          min_samples_leaf=1, min_samples_split=2,
-                          min_weight_fraction_leaf=0.0, n_estimators=100,
-                          n_iter_no_change=None, presort='deprecated',
-                          random_state=2444, subsample=1.0, tol=0.0001,
-                          validation_fraction=0.1, verbose=0, warm_start=False)
+                                     init=None, learning_rate=0.1, loss='ls', max_depth=3,
+                                     max_features=None, max_leaf_nodes=None,
+                                     min_impurity_decrease=0.0, min_impurity_split=None,
+                                     min_samples_leaf=1, min_samples_split=2,
+                                     min_weight_fraction_leaf=0.0, n_estimators=100,
+                                     n_iter_no_change=None, presort='deprecated',
+                                     random_state=2444, subsample=1.0, tol=0.0001,
+                                     validation_fraction=0.1, verbose=0, warm_start=False)
 
 modeloRF = RandomForestRegressor(bootstrap=True, ccp_alpha=0.0, criterion='mse',
-                      max_depth=None, max_features='auto', max_leaf_nodes=None,
-                      max_samples=None, min_impurity_decrease=0.0,
-                      min_impurity_split=None, min_samples_leaf=1,
-                      min_samples_split=2, min_weight_fraction_leaf=0.0,
-                      n_estimators=100, n_jobs=-1, oob_score=False,
-                      random_state=2444, verbose=0, warm_start=False)
+                                 max_depth=None, max_features='auto', max_leaf_nodes=None,
+                                 max_samples=None, min_impurity_decrease=0.0,
+                                 min_impurity_split=None, min_samples_leaf=1,
+                                 min_samples_split=2, min_weight_fraction_leaf=0.0,
+                                 n_estimators=100, n_jobs=-1, oob_score=False,
+                                 random_state=2444, verbose=0, warm_start=False)
 
 
 modelo_options = ['Extra Trees', 'Gradient Boosting', 'Random Forest']
